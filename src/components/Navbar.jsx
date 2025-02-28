ad7b49ccb509faa71105fbb3d2ec05aa67a41d9e
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2025-02-28 14:07:29 -0800

    added logos for devtinder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,57 @@
-
-const Navbar = () =>{
-    return <div className="navbar bg-base-200">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">devTinder</a>
-        </div>
-        <div className="flex gap-2">
-          
-          <div className="dropdown dropdown-end mx-5">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10  rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-              </div>
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-200">
+      <div className="flex-1">
+       
+        <img
+          className="btn btn-ghost "
+          src="src/assets/DarkThemeLogo.png"
+          alt="logo"
+        />
+      </div>
+      <div className="flex gap-2 items-center">
+      <svg
+              class="swap-on h-8 w-8 fill-current "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"></path>
+            </svg>
+        <div className="dropdown dropdown-end mx-5">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            
+            <div className="w-10  rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
-            </ul>
           </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
         </div>
       </div>
-}
+    </div>
+  );
+};
 
 export default Navbar;
