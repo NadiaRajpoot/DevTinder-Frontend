38d0199197ae0a9762d3a633fa4220d729d94ced
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2025-04-03 21:42:11 -0700

    configured redux-store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-
 import Body from "./components/Body";
 import "./index.css";
 import Login from "./components/Login";
-
 import Profile from "./components/Profile";
-
+import { Provider } from "react-redux";
+import appStore from "../utils/appStore";
 function App() {
   return (
     <>
+    <Provider store={appStore}>
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Body />}>
@@ -17,6 +17,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      </Provider>
     </>
   );
 }
